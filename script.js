88573b81eb509e22911921a5d7a99f1940ed3ca5
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2022-12-31 00:20:59 +0530

    completed game logic

--- script.js
+++ script.js
@@ -237,7 +237,7 @@ window.addEventListener("load", function () {
       this.y = Math.random() * (this.game.height * 0.95 - this.height);
       this.image = document.getElementById("angler1");
       this.frameY = Math.floor(Math.random() * 3);
-      this.lives = 2;
+      this.lives = 5;
       this.score = this.lives;
     }
   }
@@ -249,7 +249,7 @@ window.addEventListener("load", function () {
       this.y = Math.random() * (this.game.height * 0.95 - this.height);
       this.image = document.getElementById("angler2");
       this.frameY = Math.floor(Math.random() * 2);
-      this.lives = 3;
+      this.lives = 6;
       this.score = this.lives;
     }
   }
@@ -261,7 +261,7 @@ window.addEventListener("load", function () {
       this.y = Math.random() * (this.game.height * 0.95 - this.height);
       this.image = document.getElementById("lucky");
       this.frameY = Math.floor(Math.random() * 2);
-      this.lives = 3;
+      this.lives = 5;
       this.score = 15;
       this.type = "lucky";
     }
@@ -274,7 +274,7 @@ window.addEventListener("load", function () {
       this.y = Math.random() * (this.game.height * 0.95 - this.height);
       this.image = document.getElementById("hivewhale");
       this.frameY = 0;
-      this.lives = 15;
+      this.lives = 20;
       this.score = this.lives;
       this.type = "hive";
       this.speedX = Math.random() * -1.2 - 0.2;
@@ -339,7 +339,12 @@ window.addEventListener("load", function () {
     constructor(game, x, y) {
       this.game = game;
       this.frameX = 0;
+      this.spriteWidth = 200;
       this.spriteHeight = 200;
+      this.width = this.spriteWidth;
+      this.height = this.spriteHeight;
+      this.x = x - this.width * 0.5;
+      this.y = y - this.height * 0.5;
       this.fps = 30;
       this.timer = 0;
       this.interval = 1000 / this.fps;
@@ -375,14 +380,15 @@ window.addEventListener("load", function () {
       super(game, x, y);
       this.game = game;
       this.image = document.getElementById("smokeExplosion");
-      this.spriteWidth = 200;
-      this.width = this.spriteWidth;
-      this.height = this.spriteHeight;
-      this.x = x - this.width * 0.5;
-      this.y = y - this.height * 0.5;
     }
   }
-  class fireExplosion extends Explosion {}
+  class FireExplosion extends Explosion {
+    constructor(game, x, y) {
+      super(game, x, y);
+      this.game = game;
+      this.image = document.getElementById("fireExplosion");
+    }
+  }
   class UI {
     constructor(game) {
       this.game = game;
@@ -448,16 +454,16 @@ window.addEventListener("load", function () {
       this.particles = [];
       this.explosions = [];
       this.enemyTimer = 0;
-      this.enemyInterval = 1000;
+      this.enemyInterval = 1500;
       this.ammo = 20;
       this.maxAmmo = 50;
       this.ammotimer = 0;
-      this.ammoInterval = 500;
+      this.ammoInterval = 350;
       this.gameOver = false;
       this.score = 0;
-      this.winningScore = 10;
+      this.winningScore = 80;
       this.gameTime = 0;
-      this.timeLimit = 15000;
+      this.timeLimit = 30000;
       this.speed = 1;
       this.debug = false;
     }
@@ -496,7 +502,7 @@ window.addEventListener("load", function () {
             );
           }
           if (enemy.type === "lucky") this.player.enterPowerUp();
-          else this.score--;
+          else if (!this.gameOver) this.score--;
         }
         this.player.projectiles.forEach((projectile) => {
           if (this.checkCollision(projectile, enemy)) {
@@ -563,12 +569,12 @@ window.addEventListener("load", function () {
       const randomizer = Math.random();
       if (randomizer < 0.3) this.enemies.push(new Angler1(this));
       else if (randomizer < 0.6) this.enemies.push(new Angler2(this));
-      else if (randomizer < 0.8) this.enemies.push(new HiveWhale(this));
+      else if (randomizer < 0.7) this.enemies.push(new HiveWhale(this));
       else this.enemies.push(new LuckyFish(this));
     }
     addExplosion(enemy) {
       const randomizer = Math.random();
-      if (randomizer < 1)
+      if (randomizer < 0.5) {
         this.explosions.push(
           new SmokeExplosion(
             this,
@@ -576,6 +582,15 @@ window.addEventListener("load", function () {
             enemy.y + enemy.height * 0.5
           )
         );
+      } else {
+        this.explosions.push(
+          new FireExplosion(
+            this,
+            enemy.x + enemy.width * 0.5,
+            enemy.y + enemy.height * 0.5
+          )
+        );
+      }
     }
     checkCollision(rect1, rect2) {
       return (
